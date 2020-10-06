733ca5a34c5c9ba6beb13d915672ad54c2f559dd
Lukas Bausler <72468643+lbausler@users.noreply.github.com>
2020-10-06 14:06:39 -0700

    Create Driver.java

---
+++ MLLibrary/Driver.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Driver{
+  public static void main(String[] args){
+  
+    Random rand = new Random(); // creating Random object
+    
+    double [] trainingDataPoints = {0,1,2,3};
+    dataPoints[0]= rand.nextDouble(10);
+    dataPoints[1]= rand.nextDouble(10);
+    dataPoints[2]= rand.nextInt(10);
+    dataPoints[3]= rand.nextInt(10);
+  }
+}
