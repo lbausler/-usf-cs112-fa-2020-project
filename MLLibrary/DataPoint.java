a40b282c8743202127fbf4f3ffd39278a5d08611
Lukas Bausler <72468643+lbausler@users.noreply.github.com>
2020-10-06 13:36:46 -0700

    Create DataPoint.java

---
+++ MLLibrary/DataPoint.java
@@ -0,0 +1,56 @@
+public class DataPoint {
+  double f1;
+  double f2;
+  String label;
+  String type;
+  
+  public DataPoint(){
+    this.f1 = 0.0;
+    this.f2 = 0.0;
+  }
+  
+  public DataPoint(String type,String label){
+    this.type = type;
+    this.label = label; 
+    this.f1 = 0.0;
+    this.f2 = 0.0;
+  }
+  
+  
+  public void setType(String newType){
+    this.type = newType;
+  }
+  public String getType(){
+    return this.type;
+  }
+  
+  public void setLabel(String newLabel){
+    this.label = newLabel;
+  }
+  public String getLabel(){
+    return this.label;
+  }
+  
+  pubic void setf1(double newF1){
+    this.f1 = newF1;
+  } 
+  public double getF1(){
+    return this.f1;
+  }
+  
+  public void setf2(double newF2){
+    this.f2 = newF2;
+  }
+  public double getF2(){
+    return this.f2;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
