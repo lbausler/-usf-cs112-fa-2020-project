f94319d842dc7958b3502335fed512bade14229c
Lukas Bausler <72468643+lbausler@users.noreply.github.com>
2020-10-08 11:34:26 -0700

    Update Jfram.java

--- MLLibrary/Jfram.java
+++ MLLibrary/Jfram.java
@@ -2,8 +2,6 @@
 import javax.swing.*;    // all of the Swing objects
 import java.awt.*;       // more windowing components, including Container
 import java.io.*;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class projectOne {
@@ -25,22 +23,16 @@ public class projectOne {
 	// We need to set how we want our content pane to lay out the 
 	// objects we add to it.  For now, we'll use a FlowLayout, which
 	// places objects left-to-right in a line.  (See the javadocs.)
-	contentPane.setLayout(new GridLayout(2,2));
+	contentPane.setLayout(new GridLayout(1,1));
 
 	// Now we can add any Swing components we like:
 	Point2d point2d = new Point2d(3.14,5.5);
 	String str = "Luke";
 	Point2dButton p2b = new Point2dButton(str, point2d);
 	
-	//adding the image 
-	
-	
-	String path = "C:\\Users\\lbaus\\eclipse-workspace\\Assignment 2\\Lab 3 Using Java Swing Library\\src\\Skateboard.jpg";
-	Icon icon = new ImageIcon(path);
-	
 	
 	contentPane.add(p2b);
-	contentPane.add(new JButton("A Better Button?"));
-	contentPane.add(new JButton("3"));
-	contentPane.add(new JButton(icon));
+	contentPane.add(new JButton("Test Data"));
+	contentPane.add(new JButton("Train Data"));
+	
 	
