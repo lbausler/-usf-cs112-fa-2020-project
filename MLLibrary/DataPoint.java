5ddece58570cf1f5b27d8af3a74576ad662dc255
Lukas Bausler <72468643+lbausler@users.noreply.github.com>
2020-10-15 10:35:43 -0700

    Update DataPoint.java

--- MLLibrary/DataPoint.java
+++ MLLibrary/DataPoint.java
@@ -1,17 +1,18 @@
 public class DataPoint {
-  double f1;
-  double f2;
-  String label;
-  String type;
+  private double f1;
+  private double f2;
+  private String label;
+  private String type;
   
   public DataPoint(){
     this.f1 = 0.0;
     this.f2 = 0.0;
   }
   
-  public DataPoint(String type,String label, double f1, double f2){
-    this.type = type;
-    this.label = label; 
+  public DataPoint(double f1, double f2,String label,String type){
+	  
+    this.label = label;
+	this.type = type;
     this.f1 = f1;
     this.f2 = f2;
   }
@@ -24,12 +25,14 @@ public class DataPoint {
     return this.type;
   }
   
+  
   public void setLabel(String newLabel){
     this.label = newLabel;
   }
   public String getLabel(){
     return this.label;
   }
+  
   
   public void setf1(double newF1){
     this.f1 = newF1;
@@ -38,12 +41,15 @@ public class DataPoint {
     return this.f1;
   }
   
+  
   public void setf2(double newF2){
     this.f2 = newF2;
   }
   public double getF2(){
     return this.f2;
   }
+  
+  
   public String toString() {
 	  return this.type + " " + this.label + " " + this.f1 + " " + this.f2;
 	  
