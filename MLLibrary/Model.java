4d1b3ae6c1c068f558cee1f8dfbd9ed7f3320715
Lukas Bausler <72468643+lbausler@users.noreply.github.com>
2020-10-15 10:37:23 -0700

    Update Model.java

--- MLLibrary/Model.java
+++ MLLibrary/Model.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
-public abstract class Model{
-  abstract void train(ArrayList<DataPoint> data);
+public abstract class Model {
+
+  abstract void train(ArrayList<DataPoint> sampleData);
   abstract String test(ArrayList<DataPoint> data);
   abstract Double getAccuracy(ArrayList<DataPoint> data);
   abstract Double getPrecision(ArrayList<DataPoint> data);
