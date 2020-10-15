35d2542664bfb7cbee7c264da34a02efe978fe99
Lukas Bausler <72468643+lbausler@users.noreply.github.com>
2020-10-15 10:36:59 -0700

    Update DummyModel.java

--- MLLibrary/DummyModel.java
+++ MLLibrary/DummyModel.java
@@ -2,31 +2,76 @@ import java.util.ArrayList;
 
 public class DummyModel extends Model{
 
-	@Override
-	void train(ArrayList<DataPoint> data) {
-		System.out.println("Inside train method");
-		data.clear();
-        data.add(new DataPoint("type_" + 0,"label_" + 0, 3.14, 3.14));
+  private double maxRedX = 0;
+  private double maxRedY = 0;
+  private double maxBlueX = 0;
+  private double maxBlueY = 0;
 
-	}
+  //train the data
+ 
+  
+  void train(ArrayList<DataPoint> sampleData) {
+    double maxRedX = 0;
+    double maxRedY = 0;
+    double maxBlueX = 0;
+    double maxBlueY = 0;
 
-	@Override
-	String test(ArrayList<DataPoint> data) {
-		System.out.println("Inside test method");
-		data.clear();
-		return null;
-	}
+    for (int i = 0; i < sampleData.size(); i++) {
+      if (sampleData.get(i).getLabel() == "red") {
+        if (sampleData.get(i).getF1() > maxRedX) {
+          maxRedX = sampleData.get(i).getF1();
+        }
+        if (sampleData.get(i).getF2() > maxRedY) {
+          maxRedY = sampleData.get(i).getF2();
+        }
+      }
 
-	@Override
-	Double getAccuracy(ArrayList<DataPoint> data) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+      if (sampleData.get(i).getLabel() == "blue") {
+        if (sampleData.get(i).getF1() > maxBlueX) {
+          maxBlueX = sampleData.get(i).getF1();
+        }
+        if (sampleData.get(i).getF2() > maxBlueY) {
+          maxBlueY = sampleData.get(i).getF2();
+        }
+      }
+    }
+    this.maxRedX = maxRedX;
+    this.maxRedY = maxRedY;
+    this.maxBlueX = maxBlueX;
+    this.maxBlueY = maxBlueY;
+  }
+  
+  public String getMaximums() {
+	  return this.maxRedX + " " + this.maxRedY + " " +  this.maxBlueX + " " + this.maxBlueY;
+	  
+	  
+  }
+  //Test the data
+  
+  String test(ArrayList<DataPoint> data) {
+    DataPoint testData = data.get(0);
 
-	@Override
-	Double getPrecision(ArrayList<DataPoint> data) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    if (Math.abs(testData.getF1() - this.maxRedX) > Math.abs(testData.getF1() - this.maxBlueX)) {
+      return "blue";
+    }
+    else {
+      return "red";
+    }
+  }
+  
+
+@Override
+Double getAccuracy(ArrayList<DataPoint> data) {
+	// TODO Auto-generated method stub
+	return (this.maxRedX + this.maxRedY) / 2;
+}
+
+@Override
+Double getPrecision(ArrayList<DataPoint> data) {
+	// TODO Auto-generated method stub
+	return (this.maxBlueX + this.maxBlueY) / 2;
+}
+
+
   
 }
